3177e462b0e8dcf7813c99c47ce40329cfe649d8
anisart <anisart92@gmail.com>
2019-02-24 22:23:04 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,16 +1,11 @@
 def call() {
     stage('One') {
-        steps {
-            script {
                 currentBuild.displayName = currentBuild.displayName + ": VERSION_NAME"
-            }
             sh "ls -la"
         }
     }
     stage('Two') {
-        steps {
             echo "two"
-        }
     }
 }
 
