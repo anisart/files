375493aac613c774731a253af67c2fdbaa147da4
anisart <anisart92@gmail.com>
2019-02-24 22:46:54 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,14 @@
+def common = load('common.groovy')
+
 def call() {
     stage('One') {
        currentBuild.displayName = currentBuild.displayName + ": VERSION_NAME"
        sh "ls -la"
+       common.func1()
     }
     stage('Two') {
        echo "two"
+       common.func2('Qwert')
     }
 }
 
