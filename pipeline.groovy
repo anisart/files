d9f90afd35e383dd4a7292e995555e4ae93cd57e
anisart <anisart92@gmail.com>
2019-02-25 21:04:18 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,7 +1,7 @@
-def call() {
+def call(Map config) {
     def common = load('.files/common.groovy')
     // stage('Clone')
-    common.checkoutVv credentialsId: '57137eb5-c677-4b23-ad2a-510919a8e2cd'
+    common.checkoutVv credentialsId: config.gitCredentialsId
 
     stage('One') {
        currentBuild.displayName = currentBuild.displayName + ": VERSION_NAME"
