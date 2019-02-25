178aed9b75944c25b6142f3070952f1c1d507754
anisart <anisart92@gmail.com>
2019-02-25 19:37:52 +0300

    Update common.groovy

--- common.groovy
+++ common.groovy
@@ -6,4 +6,20 @@ def func2(String whoAreYou) {
   echo "Look at this, ${whoAreYou}! You loaded this from another file!"
 }
 
+def checkoutVv(Map config) {
+  checkout([
+    $class: 'GitSCM',
+    branches: [[name: 'fake']],
+    doGenerateSubmoduleConfigurations: false,
+    extensions: [
+            [$class: 'CleanBeforeCheckout'],
+            [$class: 'PruneStaleBranch'],
+            [$class: 'CloneOption',
+             noTags: false,
+             timeout: 60]],
+    submoduleCfg: [],
+    userRemoteConfigs: [[url: 'https://github.com/anisart/VV.git', credentialsId: config.credentialsId]]
+  ])
+}
+
 return this
