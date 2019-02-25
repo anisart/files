8ad22a51484f50e720eeeaab941d04a1ed6cce68
anisart <anisart92@gmail.com>
2019-02-25 19:39:44 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,5 +1,8 @@
 def call() {
     def common = load('common.groovy')
+    stage('Clone') {
+        checkoutVv credentialsId: '57137eb5-c677-4b23-ad2a-510919a8e2cd'
+    }
     stage('One') {
        currentBuild.displayName = currentBuild.displayName + ": VERSION_NAME"
        sh "ls -la"
